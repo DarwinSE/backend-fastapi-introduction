07cae89f6b8b5550b87a2156c2704a7bb7aac491
Darwin Guerra <darwin.guerra@seneca-corp.com>
2025-01-23 15:00:08 -0500

    primeras definiciones de la api, root y url

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# run del server uvicorn main:app --reload
+
+@app.get('/')
+async def root():
+    return 'Hola FastAPI'
+
+@app.get('/url')
+async def url():
+    return {'url_generica': 'https://youtube.com'}
+
+# Documentacion propia de FastAPI http://127.0.0.1:8000/docs
+# Documentacion propia de FastAPI http://127.0.0.1:8000/redoc
