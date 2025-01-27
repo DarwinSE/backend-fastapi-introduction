31df11ce806d2c3bdbee0304c7fa1a0303717ec8
Darwin Guerra <darwin.guerra@seneca-corp.com>
2025-01-27 16:09:57 -0500

    agregando un path por defecto y un tag para agrupar

--- routers/products.py
+++ routers/products.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 
-router = APIRouter() 
+router = APIRouter(prefix = '/products', tags = ['products'], responses = {404: {'message': '404 Not Found'}})
 
-@router.get('/products')
+products_list = ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4', 'Producto 5']
+
+@router.get('/')
 def products():
-    return ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4', 'Producto 5']
+    return products_list
+
+@router.get('/{product_id}')
+def product(product_id: int):
+    return products_list[product_id]
